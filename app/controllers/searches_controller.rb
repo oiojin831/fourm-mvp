ecee551e9a7a82fed7ab25d88ae1a20239728373
eungjinlee <oiojin831@gmail.com>
2015-06-04 05:27:08 +0000

    refactor parser

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,11 +1,12 @@
 class SearchesController < ApplicationController
   def index
-    @store = Store.first
+    @store = Store.all
   end
 
   def create
     data = InstaParser.new(username: params[:search])
     store_info = data.parse_store
+    #medias_info = data.parse_medias
     Store.create(username: store_info[:username], insta_id: store_info[:insta_id], profile_picture: store_info[:profile_picture])
 
     redirect_to searches_path
--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20150602014914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "stores", force: :cascade do |t|
     t.string   "username"
     t.integer  "insta_id"
