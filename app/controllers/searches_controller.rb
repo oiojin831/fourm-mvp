e1d214fa7fb7d2af618f6f23afb8722fa3c96a51
eungjinlee <oiojin831@gmail.com>
2015-06-04 09:29:45 +0000

    Refactor

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,9 +6,15 @@ class SearchesController < ApplicationController
   def create
     data = InstaParser.new(username: params[:search])
     store_info = data.parse_store
-    #medias_info = data.parse_medias
-    Store.create(username: store_info[:username], insta_id: store_info[:insta_id], profile_picture: store_info[:profile_picture])
+    # medias_info = data.parse_medias
+    store = Store.new do |s|
+      s.username = store_info[:username]
+      s.insta_id = store_info[:insta_id]
+      s.profile_picture = store_info[:profile_picture]
+    end
 
-    redirect_to searches_path
+    if store.save
+      redirect_to searches_path
+    end
   end
 end
