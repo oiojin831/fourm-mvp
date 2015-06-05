f121cf79a656d3b4ddf0aed5a621bb135b9a8708
eungjinlee <oiojin831@gmail.com>
2015-06-05 01:44:09 +0000

    Move db creation out of ctrl to InstaWorker

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,32 +4,11 @@ class SearchesController < ApplicationController
   end
 
   def create
-    data = InstaParser.new(username: params[:search])
-    store_info = data.parse_store
-    media_info = data.parse_media[0]
-
-    store = Store.new do |s|
-
-      s.username = store_info[:username]
-      s.insta_id = store_info[:insta_id]
-      s.profile_picture = store_info[:profile_picture]
-    end
-
-    if store.save
-      medium = Medium.new do |m|
-        m.store_id = store.id
-        m.caption_text = media_info[:caption_text]
-        m.low_resolution = media_info[:low_resolution][:url]
-        m.thumbnail = media_info[:thumbnail][:url]
-        m.standard_resolution = media_info[:standard_resolution][:url]
-      end
-
-      if medium.save
-        redirect_to searches_path
-      end
-
-    else
+    worker = InstaWorker.new(username: params[:search])
+    if worker.save
       redirect_to searches_path
+    else
+      redirect_to root_path
     end
 
   end
---
+++ app/models/insta_worker.rb
@@ -0,0 +1,37 @@
+class InstaWorker
+  def initialize(username: )
+    @username = username
+  end
+
+  def save
+    parse
+
+    store = Store.new do |s|
+      s.username = @store_info[:username]
+      s.insta_id = @store_info[:insta_id]
+      s.profile_picture = @store_info[:profile_picture]
+    end
+
+    if store.save
+      medium = Medium.new do |m|
+        m.store_id = store.id
+        m.caption_text = @media_info[:caption_text]
+        m.low_resolution = @media_info[:low_resolution][:url]
+        m.thumbnail = @media_info[:thumbnail][:url]
+        m.standard_resolution = @media_info[:standard_resolution][:url]
+      end
+
+      medium.save
+    else
+      false
+    end
+  end
+
+  private
+
+  def parse
+    data = InstaParser.new(username: @username)
+    @store_info = data.parse_store
+    @media_info = data.parse_media[0]
+  end
+end
